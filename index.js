5ecea5d41c7a6440a63cbc279e7b34a660c37189
Alisher Zakir <a3zakir@gmail.com>
2019-04-18 23:04:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var arr = [];
+  for (var i = 0; i < musicians.length; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return arr;
+}
+
+function johnLennonFacts(facts) {}
